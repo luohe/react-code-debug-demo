bfb1f26485a6b7cadc5a35f172c2672611f83772
xiazhiqiang14 <xiazhiqiang14@jd.com>
2024-09-19 22:42:38 +0800

    chore: update env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello, React 18!</h1>
+  <h1>Hello, React!</h1>
 );
 
 // If you want to start measuring performance in your app, pass a function
