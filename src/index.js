88e91929fc69dc708253cfc0043bf47d35b7babf
luohe <crazy_zhiqiang@sina.com>
2024-09-19 22:47:57 +0800

    feat: remove conflict

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< Updated upstream
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import App from './App';
->>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
